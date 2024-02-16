af26c4083fc973b08e8045d2b48e30535ac51014
Andre Menezes <djdedemenezes@gmail.com>
2024-02-16 18:05:42 -0300

    run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_16_191426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.integer "favorite_shirt_number"
+    t.integer "shoe_size"
+    t.integer "number_of_children"
+    t.integer "height"
+    t.integer "weight"
+    t.string "position"
+    t.string "birth_city"
+    t.integer "minimum_income"
+    t.integer "maximum_income"
+    t.string "favorite_food"
+    t.string "favorite_drink"
+    t.string "preferred_look"
+    t.string "favorite_videogame"
+    t.integer "membership_duration"
+    t.string "sport_interest"
+    t.boolean "merchandising_interest"
+    t.string "preferred_communication_channel"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "favorite_shirt_number"
+    t.integer "shoe_size"
+    t.integer "number_of_children"
+    t.integer "height"
+    t.integer "weight"
+    t.string "birth_city"
+    t.integer "minimum_income"
+    t.integer "maximum_income"
+    t.string "favorite_food"
+    t.string "favorite_drink"
+    t.string "preferred_look"
+    t.string "favorite_videogame"
+    t.integer "membership_duration"
+    t.string "sport_interest"
+    t.boolean "merchandising_interest"
+    t.string "preferred_communication_channel"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
