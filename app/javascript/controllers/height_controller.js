878efa0307a2e1d19dc3d95a72f5fa79764ffef3
Andre Menezes <djdedemenezes@gmail.com>
2024-02-17 12:13:39 -0300

    update value to convert input to pixel

--- app/javascript/controllers/height_controller.js
+++ app/javascript/controllers/height_controller.js
@@ -9,7 +9,7 @@ export default class extends Controller {
   }
 
   match() {
-    this.imageTarget.style.width = `${this.inputTarget.value * 2}px`;
+    this.imageTarget.style.width = `${this.inputTarget.value * 1.75}px`;
   }
 }
 
