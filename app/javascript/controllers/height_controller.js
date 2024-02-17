dd014cf9a15fd06219666fc37a97aef7d67a6c4f
Andre Menezes <djdedemenezes@gmail.com>
2024-02-17 14:09:30 -0300

    update new height view using JS to change image when fanmatch is pressed

--- app/javascript/controllers/height_controller.js
+++ app/javascript/controllers/height_controller.js
@@ -2,14 +2,35 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="height"
 export default class extends Controller {
-  static targets = ['input', 'image']
+  static targets = ['input', 'image', 'match', 'submit', 'card', 'name', 'result', 'imageCard']
   connect() {
     console.log(this.inputTarget)
-    console.log(this.imageTarget)
+    console.log(this.imageCardTarget)
+  }
+
+  updateSize() {
+    this.imageTarget.style.width = `${this.inputTarget.value * 1.75}px`;
   }
 
   match() {
-    this.imageTarget.style.width = `${this.inputTarget.value * 1.75}px`;
+    // fazer fetch
+    fetch(`/fan/heights?number=${this.inputTarget.value}`)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+        // esconder match button
+        this.matchTarget.classList.add('d-none')
+        // ativar botao de submit
+        this.submitTarget.classList.remove('d-none')
+        this.submitTarget.disabled = false
+        // Esconder imagem padrao default
+        this.imageTarget.classList.add('d-none')
+        this.nameTarget.innerText = data.shirt_name
+        // alterar src da imagem
+        this.imageCardTarget.setAttribute('src', data.body_image_url)
+        this.resultTarget.innerText = `${this.inputTarget.value}cm`
+        this.cardTarget.classList.remove('d-none')
+      })
   }
 }
 
