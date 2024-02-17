c98d3ea6e30d0fbf67eede4aa73fffdfe4e5d9cc
Caio Alves <caioalvesfig@gmail.com>
2024-02-17 17:00:37 -0300

    edde

--- app/controllers/fan/matches_controller.rb
+++ app/controllers/fan/matches_controller.rb
@@ -2,6 +2,6 @@ class Fan::MatchesController < ApplicationController
   def show
     @profile = current_user.profile
     @best_match = current_user.find_best_match(Player.all)
-      flash.now[:notice] = 'Aproveit seu desconto único'
+      flash.now[:notice] = 'Aproveite seu desconto único'
   end
 end
