87deeb671fef6f4c670afc829a9dcfa4e9380e67
Andre Menezes <djdedemenezes@gmail.com>
2024-02-17 16:39:54 -0300

    fixing inpiut

--- app/models/fan/favorite_food.rb
+++ app/models/fan/favorite_food.rb
@@ -1,6 +1,7 @@
 module Fan
   class FavoriteFood
     include ActiveModel::Model
+  OPTIONS = ['Bacalhau', 'Feijoada', 'Churrasco', 'Pizza', 'Tacos', 'Lasanha', 'Sushi', 'Hambúrguer', 'Acarajé']
 
     attr_accessor :string
     validates :string, presence: true
