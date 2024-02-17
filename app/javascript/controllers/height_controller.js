9173abbbcdad7a63000a0112ecaca49e73edf932
Andre Menezes <djdedemenezes@gmail.com>
2024-02-17 11:56:17 -0300

    add sitmulus contoller to match image size to iunput value

---
+++ app/javascript/controllers/height_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="height"
+export default class extends Controller {
+  static targets = ['input', 'image']
+  connect() {
+    console.log(this.inputTarget)
+    console.log(this.imageTarget)
+  }
+
+  match() {
+    this.imageTarget.style.width = `${this.inputTarget.value}px`;
+  }
+}
+
+
+// const image = document.getElementById('imagem');
+// const input = document.querySelector('#height')
+// image.style.width = input.value
+// input.addEventListener('change', (event) => {
+// })
